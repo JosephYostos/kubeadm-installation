6e4ce3adf566988a7e6b6c31f4fbb55871c88abf
JosephYostos <86612715+JosephYostos@users.noreply.github.com>
2023-01-20 15:06:17 +0000

    Update master.sh

--- master.sh
+++ master.sh
@@ -15,7 +15,7 @@ sudo apt install -y \
           kubelet=${K8SVERSION} \
           kubectl=${K8SVERSION} 
 
-# install required repo for containerd
+# Install the necessary dependencies for containerd
 sudo apt install curl gnupg2 software-properties-common apt-transport-https ca-certificates -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
