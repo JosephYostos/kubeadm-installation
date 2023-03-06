ba1208eedf8c1853fb5200897da28e1a476df1e3
JosephYostos <86612715+JosephYostos@users.noreply.github.com>
2023-03-06 14:46:16 +0000

    Update master_125.sh

--- master_125.sh
+++ master_125.sh
@@ -68,5 +68,5 @@ sudo kubeadm init \
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-#kubectl taint nodes --all node-role.kubernetes.io/master-
-#kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
