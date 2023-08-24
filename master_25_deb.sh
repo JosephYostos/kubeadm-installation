7460608dae7223bc709adfb00b78c77e74a3f5ae
JosephYostos <86612715+JosephYostos@users.noreply.github.com>
2023-08-24 12:41:48 +0100

    Update master_25_deb.sh

--- master_25_deb.sh
+++ master_25_deb.sh
@@ -27,7 +27,7 @@ sudo apt install -y \
 
 # Install the necessary dependencies for containerd
 sudo apt install curl gnupg2 software-properties-common apt-transport-https ca-certificates -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" |sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update
 
