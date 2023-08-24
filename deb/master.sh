00749590c543624e0ac3fb5ca510d3b31f73a5c6
JosephYostos <86612715+JosephYostos@users.noreply.github.com>
2023-08-24 13:56:32 +0100

    Update master.sh

--- deb/master.sh
+++ deb/master.sh
@@ -2,6 +2,7 @@
 
 
 sudo apt-get update -y
+sudo apt-get -y install gnupg
 # apt-transport-https may be a dummy package; if so, you can skip that package
 sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
